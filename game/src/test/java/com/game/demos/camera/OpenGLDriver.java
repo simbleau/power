c9599689db0b2c4702186571accb7b5d1414926d
Spencer Imbleau <spencer@imbleau.com>
2020-12-27 08:51:11 -0500

    Simplified the camera demo drivers

--- game/src/test/java/com/game/demos/camera/OpenGLDriver.java
+++ game/src/test/java/com/game/demos/camera/OpenGLDriver.java
@@ -1,14 +1,10 @@
 package com.game.demos.camera;
 
 import com.game.demos.artifacts.DemoGame;
+import com.game.demos.util.DemoDisplaySupport;
 import com.game.demos.util.DemoDriverSupport;
-import com.game.engine.camera.DevCamera;
-import com.game.engine.display.DisplaySettingsFactory;
 import com.game.engine.driver.GameDriver;
-import com.game.engine.driver.GameDriverFactory;
 import com.game.engine.game.AbstractGame;
-import com.game.engine.game.Chunk;
-import com.game.engine.rendering.common.RenderMode;
 
 /**
  * A demo driver to test camera affecting coordinates.
@@ -29,22 +25,8 @@ public class OpenGLDriver {
 	 * @param args - CLI args
 	 */
 	public static void main(String[] args) {
-		// Setup the game driver settings
-		GameDriverFactory gdF = new GameDriverFactory();
-		gdF.setTps(DemoDriverSupport.DEFAULT_TPS);
-		gdF.setCache(DemoDriverSupport.DEFAULT_CACHE);
-		gdF.setGame(DEMO_GAME);
-
-		// Setup the display settings
-		DisplaySettingsFactory dsF = new DisplaySettingsFactory();
-		dsF.setMode(RenderMode.OPENGL);
-		dsF.setResolutionWidth(480);
-		dsF.setResolutionHeight(360);
-		dsF.setCamera(new DevCamera(0, 0, 0, 0, 1));
-
-		// Start the game
-		GameDriver driver = gdF.get();
-		driver.init(dsF.get());
+		GameDriver driver = DemoDriverSupport.getDefault(DEMO_GAME);
+		driver.init(DemoDisplaySupport.getDefaultOpenGL());
 		driver.start();
 	}
 }
